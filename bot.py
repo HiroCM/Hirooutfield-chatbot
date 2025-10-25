322ce1a20868399e131dec05ad6ca5cfb041d279
Hiro Razali <hiro@Hiros-MacBook-Pro.local>
2025-10-25 14:23:06 +0800

    Fix: reuse existing asyncio loop for Render environment

--- bot.py
+++ bot.py
@@ -298,4 +298,13 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        if "event loop is already running" in str(e):
+            loop = asyncio.get_event_loop()
+            loop.create_task(main())
+            loop.run_forever()
+        else:
+            raise
